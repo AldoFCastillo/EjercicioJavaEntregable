2f8bfda533e873a43a00068d22c10cf10bf7121b
Aldo <aldocastillomdq@gmail.com>
2019-09-07 21:09:32 -0300

    Parte E
    Se crean nuevos atributos para clase Curso. Profesor titular, prof adjunto y lista de alumnos

--- src/com/company/Curso.java
+++ src/com/company/Curso.java
@@ -1,13 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
 
     private String nombre;
     private Integer codigoDeCurso;
+    private ProfesorTitular profesorTitular;
+    private ProfesorAdjunto profesorAdjunto;
+    private List<Alumno> alumnosInscriptos;
 
-    public Curso(String nombre, Integer codigoDeCurso) {
+    public Curso(String nombre, Integer codigoDeCurso, ProfesorTitular profesorTitular, ProfesorAdjunto profesorAdjunto) {
         this.nombre = nombre;
         this.codigoDeCurso = codigoDeCurso;
+        this.profesorTitular = profesorTitular;
+        this.profesorAdjunto = profesorAdjunto;
+        this.alumnosInscriptos = new ArrayList<>();
     }
 
     public String getNombre() {
@@ -16,6 +25,31 @@ public class Curso {
 
     public Integer getCodigoDeCurso() {
         return codigoDeCurso;
+    }
+
+    public ProfesorTitular getProfesorTitular() {
+        return profesorTitular;
+    }
+
+    public ProfesorAdjunto getProfesorAdjunto() {
+        return profesorAdjunto;
+    }
+
+    public List<Alumno> getAlumnosInscriptos() {
+        return alumnosInscriptos;
+    }
+
+    public void setProfesorTitular(ProfesorTitular profesorTitular) {
+        this.profesorTitular = profesorTitular;
+    }
+
+    public void setProfesorAdjunto(ProfesorAdjunto profesorAdjunto) {
+        this.profesorAdjunto = profesorAdjunto;
+    }
+
+
+    public void setAlumnosInscriptos(List<Alumno> alumnosInscriptos) {
+        this.alumnosInscriptos = alumnosInscriptos;
     }
 
     public void setNombre(String nombre) {
