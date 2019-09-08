62b3c9feb65282ba687c2d27e1c67e9c053175c5
Aldo <aldocastillomdq@gmail.com>
2019-09-08 11:44:05 -0300

    Parte H
    En clase Curso, se crean los metodos para agregar alumno , eliminar alumno, buscar repetidos y confirmar cupo disponible.

--- src/com/company/Curso.java
+++ src/com/company/Curso.java
@@ -11,6 +11,8 @@ public class Curso {
     private ProfesorAdjunto profesorAdjunto;
     private List<Alumno> alumnosInscriptos;
 
+    private Integer cantidadDeAlumnosPermitidos;
+
     public Curso(String nombre, Integer codigoDeCurso, ProfesorTitular profesorTitular, ProfesorAdjunto profesorAdjunto) {
         this.nombre = nombre;
         this.codigoDeCurso = codigoDeCurso;
@@ -39,6 +41,14 @@ public class Curso {
         return alumnosInscriptos;
     }
 
+    public Integer getCantidadDeAlumnosPermitidos() {
+        return cantidadDeAlumnosPermitidos;
+    }
+
+    public void setCantidadDeAlumnosPermitidos(Integer cantidadDeAlumnosPermitidos) {
+        this.cantidadDeAlumnosPermitidos = cantidadDeAlumnosPermitidos;
+    }
+
     public void setProfesorTitular(ProfesorTitular profesorTitular) {
         this.profesorTitular = profesorTitular;
     }
@@ -62,7 +72,78 @@ public class Curso {
 
     @Override
     public boolean equals(Object obj) {
+        if (!(obj instanceof Curso)) {
+            return false;
+        }
         Curso unCurso = (Curso) obj;
         return unCurso.getCodigoDeCurso().equals(this.codigoDeCurso);
     }
+
+
+    /**
+     * Metodo para agregar alumnos al curso
+     *
+     * @param unAlumno
+     * @return
+     */
+    public Boolean agregarUnAlumno(Alumno unAlumno) {
+
+        if (!hayCupoDisponible() && !alumnoRepetido(unAlumno)) {
+            System.out.println("No se pudo realizar la operacion");
+            return false;
+        }
+        alumnosInscriptos.add(unAlumno);
+        return true;
+    }
+
+    /**
+     * Metodo para verificar cupo
+     *
+     * @return
+     */
+
+    public Boolean hayCupoDisponible() {
+        Boolean hayCupo = true;
+        if (!(cantidadDeAlumnosPermitidos > alumnosInscriptos.size())) {
+            hayCupo = false;
+            System.out.println("No hay cupo disponible");
+        }
+        return hayCupo;
+    }
+
+    /**
+     * Metodo para verificar si el alumno esta repetido
+     *
+     * @param unAlumno
+     * @return
+     */
+
+    public Boolean alumnoRepetido(Alumno unAlumno) {
+        Boolean repetido = false;
+        for (Alumno otroAlumno : alumnosInscriptos) {
+            if (otroAlumno.equals(unAlumno)) {
+                repetido = true;
+                System.out.println("EL alumno ya fue ingresado previamente");
+                break;
+            }
+        }
+        return repetido;
+    }
+
+    /**
+     * Metodo para eliminar alumnos del curso
+     *
+     * @param unAlumno
+     */
+    public void eliminarAlumno(Alumno unAlumno) {
+        List<Alumno> alumnosAEliminar = new ArrayList<>();
+        for (Alumno otroAlumno : alumnosInscriptos) {
+            if (otroAlumno.equals(unAlumno)) {
+                alumnosAEliminar.add(unAlumno);
+            }
+        }
+        alumnosInscriptos.removeAll(alumnosAEliminar);
+        System.out.println("El alumno se ha eliminado");
+    }
 }
+
